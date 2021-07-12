d358b2f03ac1f20dd4f798a7a095b219266ab8d2
julimorozova <yuli.sleptsova@gmail.com>
2021-07-12 13:28:43 +0300

    feat: implement the adding one static square

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 let $start = document.querySelector('#start');
 let $game = document.querySelector('#game');
 
-$start.addEventListener('click', startGame);
+let score = 0;
 
+$start.addEventListener('click', startGame);
+$game.addEventListener('click', handlBoxClick);
 
 function startGame() {
     $game.style.backgroundColor = '#fff';
@@ -12,5 +14,23 @@ function startGame() {
 }
 
 function renderBox() {
-    
+    $game.innerHTML = '';
+    let box = document.createElement('div');
+
+    box.style.height = box.style.width = '50px';
+    box.style.position = 'absolute';
+    box.style.backgroundColor = '#000';
+    box.style.top = '50px';
+    box.style.left = '75px';
+    box.style.cursor = 'pointer';
+    box.setAttribute('data-box', true);
+
+    $game.insertAdjacentElement('afterbegin', box);
+}
+
+function handlBoxClick(event) {
+    if (event.target.dataset) {
+        score++;
+        renderBox();
+    }
 }
