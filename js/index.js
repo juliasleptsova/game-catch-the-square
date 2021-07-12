7c143696974de4f95bc90954905e66914a66a964
julimorozova <yuli.sleptsova@gmail.com>
2021-07-12 13:14:04 +0300

    feat: implement the start of the game

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let $start = document.querySelector('#start');
+let $game = document.querySelector('#game');
+
+$start.addEventListener('click', startGame);
+
+
+function startGame() {
+    $game.style.backgroundColor = '#fff';
+    $start.classList.add('hide');
+
+    renderBox();
+}
+
+function renderBox() {
+    
+}
